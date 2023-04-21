0d01465915f38c46ba410f72604275ac08e75328
Maël Quéau <mael.queau99@gmail.com>
2023-04-21 17:11:59 +0200

    feat: only display dev warning in production, and only display stats in development

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import DevelopmentWarning from "./components/DevelopmentWarning";
 import Header from "./components/Header";
+import { Stats } from "@react-three/drei";
 
 function App() {
 	return (
 		<>
-			<DevelopmentWarning />
+			{process.env.NODE_ENV === "production" && <DevelopmentWarning />}
+			{process.env.NODE_ENV === "development" && <Stats />}
 			<Header />
 		</>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Stats from "three/examples/jsm/libs/stats.module.js";
 import App from "./App";
 import "./index.css";
-
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
-function statsUpdate() {
-	stats.update();
-	requestAnimationFrame(statsUpdate);
-}
-
-requestAnimationFrame(statsUpdate);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
