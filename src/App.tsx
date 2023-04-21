1e6c05d8c8e3edfb22ddb4c12a633c6b746495e4
Maël Quéau <mael.queau99@gmail.com>
2023-04-21 15:42:20 +0200

    feat: add a development warning at the bottom of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import DevelopmentWarning from "./components/DevelopmentWarning";
 import Header from "./components/Header";
 
 function App() {
 	return (
 		<>
+			<DevelopmentWarning />
 			<Header />
 		</>
 	);
