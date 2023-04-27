870eeca22c05b0e3a558117b4b56f959f90a8d67
Maël Quéau <mael.queau99@gmail.com>
2023-04-27 17:09:54 +0200

    feat: tweak sections

--- src/components/Section/Body.tsx
+++ src/components/Section/Body.tsx
@@ -4,7 +4,7 @@ export interface SectionBodyProps {
 
 export default function SectionBody(props: SectionBodyProps) {
 	return (
-		<div className="leading-7 text-lg font-light text-center">
+		<div className="text-lg text-center w-full max-w-screen-lg">
 			{props.children}
 		</div>
 	);
