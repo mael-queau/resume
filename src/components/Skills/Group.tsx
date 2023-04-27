aac143385dcdc3cc487c36ae48826c3955b2e546
Maël Quéau <mael.queau99@gmail.com>
2023-04-27 20:19:10 +0200

    feat: better flexbox for skills

--- src/components/Skills/Group.tsx
+++ src/components/Skills/Group.tsx
@@ -9,10 +9,12 @@ export interface SkillGroupProps {
 
 export default function SkillGroup(props: SkillGroupProps) {
 	return (
-		<div className="flex flex-col justify-start items-center gap-2 flex-initial w-full max-w-md bg-slate-700 rounded px-2 py-5">
+		<div className="flex flex-col justify-start items-center gap-3 flex-initial w-full min-w-min max-w-md mx-auto bg-slate-700 rounded px-2 py-5">
 			<h3 className="text-center text-xl font-bold">{props.name}</h3>
 			<hr className="w-2/3" />
-			<div className="flex flex-wrap gap-2 w-full px-10">{props.children}</div>
+			<div className="flex flex-wrap gap-2 justify-center">
+				{props.children}
+			</div>
 		</div>
 	);
 }
--- src/components/Skills/Item.tsx
+++ src/components/Skills/Item.tsx
@@ -5,7 +5,7 @@ export interface SkillItemProps {
 
 export default function SkillItem(props: SkillItemProps) {
 	return (
-		<div className="flex justify-start items-center gap-2 basis-1/3 flex-auto px-2 py-1 rounded hover:bg-slate-500 hover:shadow-2xl shadow-black transition-all hover:scale-105">
+		<div className="flex items-center gap-2 w-2/5 min-w-fit sm:flex-initial py-1 px-2 rounded hover:bg-slate-500 transition-all hover:scale-105">
 			{props.icon}
 			<p className="font-mono">{props.name}</p>
 		</div>
