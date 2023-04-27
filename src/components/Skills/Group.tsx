5671ad680cd28645b1eb944f7650804bfdc22a1a
Maël Quéau <mael.queau99@gmail.com>
2023-04-27 19:46:38 +0200

    feat: add vertical padding

--- src/components/Skills/Group.tsx
+++ src/components/Skills/Group.tsx
@@ -9,7 +9,7 @@ export interface SkillGroupProps {
 
 export default function SkillGroup(props: SkillGroupProps) {
 	return (
-		<div className="flex flex-col justify-start items-center gap-2 flex-initial w-full max-w-md bg-slate-700 rounded p-2">
+		<div className="flex flex-col justify-start items-center gap-2 flex-initial w-full max-w-md bg-slate-700 rounded px-2 py-5">
 			<h3 className="text-center text-xl font-bold">{props.name}</h3>
 			<hr className="w-2/3" />
 			<div className="flex flex-wrap gap-2 w-full px-10">{props.children}</div>
