58dabbf58b41e6e074db25077e314767b5c4c26f
Maël Quéau <mael.queau99@gmail.com>
2023-05-12 20:47:32 +0200

    feat: add dismiss button to development warning

--- src/components/DevelopmentWarning.tsx
+++ src/components/DevelopmentWarning.tsx
@@ -1,6 +1,14 @@
+import { IconCross, IconCrossOff, IconX } from "@tabler/icons-react";
+import { useState } from "react";
+
 export default function DevelopmentWarning() {
+	const [dismissed, setDismissed] = useState(false);
 	return (
-		<div className="fixed bottom-0 left-0 w-full bg-red-500 text-white text-center py-2 z-10">
+		<div
+			className={`fixed bottom-0 left-0 w-full bg-red-500 text-white text-center py-2 z-10 ${
+				dismissed ? "hidden" : "block"
+			}`}
+		>
 			<p className="text-sm">
 				<strong>Warning:</strong> This site is still under development. <br />
 				The previous version can be seen{" "}
@@ -8,6 +16,12 @@ export default function DevelopmentWarning() {
 					here
 				</a>
 			</p>
+			<button
+				className="text-sm underline absolute right-8 top-1/2 -translate-y-1/2"
+				onClick={() => setDismissed(true)}
+			>
+				<IconX size={16} />
+			</button>
 		</div>
 	);
 }
