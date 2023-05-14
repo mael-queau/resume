8af0b702c2f966fd00ae6bb11532ea1b1f547047
Maël Quéau <mael.queau99@gmail.com>
2023-05-14 18:29:28 +0200

    feat: remove scale effect on skill hover

--- src/components/Skills/Item.tsx
+++ src/components/Skills/Item.tsx
@@ -5,7 +5,7 @@ export interface SkillItemProps {
 
 export default function SkillItem(props: SkillItemProps) {
 	return (
-		<div className="flex items-center gap-2 w-2/5 min-w-fit sm:flex-initial py-1 px-2 rounded hover:bg-slate-500 transition-all hover:scale-105">
+		<div className="flex items-center gap-2 w-2/5 min-w-fit sm:flex-initial py-1 px-2 rounded hover:bg-slate-500 transition-all">
 			{props.icon}
 			<p className="font-mono">{props.name}</p>
 		</div>
