e85c3886abe9a337c476910fc6377cb3ee33d9ba
Maël Quéau <mael.queau99@gmail.com>
2023-05-17 13:59:55 +0200

    chore: fix banner height on mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,8 @@ export default {
 		},
 	},
 	plugins: [],
+	mounted: function () {
+		const vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+	},
 };
